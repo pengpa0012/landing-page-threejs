8ca6e0b73b8a3d1e25a697cb795ea48623395767
Pengpa0012 <peng.ilikha@gmail.com>
2023-03-12 23:05:55 +0800

    change to fullscreen

--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,15 @@ function App() {
   ])
   return (
     <div className="App">
-      <div className="min-h-screen">
-        <Mesh 
+      <div className="relative">
+        <Mesh
           component={
           <>
-            <sphereGeometry args={[1, 50, 50]} />
+            <sphereGeometry args={[1, 50, 50]}  />
             <meshStandardMaterial normalMap={rockNormal} map={rockMap} roughnessMap={rockRoughness} />
           </>
         } position={[0, 0, 0]} scale={[2,2,2]} />
+        <h1 className="text-center text-7xl font-bold text-white absolute z-10 inset-0 grid place-items-center">GOATED</h1>
       </div>
     </div>
   )
--- src/Components/Mesh.tsx
+++ src/Components/Mesh.tsx
@@ -4,15 +4,14 @@ import { useRef, useState } from "react"
 
 export const Mesh = (props: any) => {
   const mesh = useRef()
-  
   return (
    
-    <Canvas style={{ height: 600, background: "#000" }}>
+    <Canvas style={{ height: "100vh", background: "#000" }}>
       <mesh
         {...props}
         ref={mesh}>
         {props.component}
-        <ambientLight intensity={.05} />
+        <ambientLight intensity={.1} />
         <pointLight position={[10, 0, 7]} />
         <OrbitControls autoRotate enableRotate={false} enableZoom={false} />
       </mesh>

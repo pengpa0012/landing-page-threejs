4faae0836fa239f500bbabdf5e191e8ddbea7b45
Pengpa0012 <peng.ilikha@gmail.com>
2023-03-14 07:48:36 +0800

    added scroll function on mesh

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Mesh } from './Components/Mesh'
 import { OrbitControls } from '@react-three/drei'
 import { RockBasicColor, RockNormal, RockRoughness } from './assets'
 import { TextureLoader } from 'three'
+import { useEffect, useState } from 'react'
+import { config, useSpring } from '@react-spring/three'
 
 function App() {
   const [rockMap, rockNormal, rockRoughness] = useLoader(TextureLoader, [
@@ -11,18 +13,43 @@ function App() {
     RockNormal,
     RockRoughness,
   ])
+  const [size, setSize] = useState(1)
+  const [active, setActive] = useState(false);
+
+  const { scale } = useSpring({
+    scale: size
+  });
+  
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setSize(window.scrollY * 0.005 + 1)
+    }
+
+    window.addEventListener('wheel', handleScroll)
+
+    return () => {
+      window.removeEventListener('wheel', handleScroll)
+    }
+  }, [])
+  console.log(scale)
+  // TODO
+  // Scroll event with model resizing
   return (
     <div className="App">
       <div className="relative">
-        <Mesh
-          component={
-          <>
-            <sphereGeometry args={[1, 50, 50]}  />
-            <meshStandardMaterial normalMap={rockNormal} map={rockMap} roughnessMap={rockRoughness} />
-          </>
-        } position={[0, 0, 0]} scale={[2,2,2]} />
-        <h1 className="text-center text-7xl font-bold text-white absolute z-10 inset-0 grid place-items-center">GOATED</h1>
+        <Canvas style={{ height: "100vh", background: "#000" }}>
+          <Mesh
+          onClick={() => setActive(!active)}
+            component={
+            <>
+              <sphereGeometry args={[1, 50, 50]}  />
+              <meshStandardMaterial normalMap={rockNormal} map={rockMap} roughnessMap={rockRoughness} />
+            </>
+          } position={[0, 0, 0]} scale={scale} />
+        </Canvas>
       </div>
+      <div className="min-h-screen"></div>
     </div>
   )
 }
--- src/Components/Mesh.tsx
+++ src/Components/Mesh.tsx
@@ -1,20 +1,24 @@
+import { animated } from "@react-spring/three"
 import { OrbitControls } from "@react-three/drei"
 import { Canvas, useFrame } from "@react-three/fiber"
 import { useRef, useState } from "react"
 
 export const Mesh = (props: any) => {
   const mesh = useRef()
+
+  useFrame(({ clock }) => {
+    const a = clock.getElapsedTime();
+    mesh.current!.rotation.y = a;
+  });
+
   return (
-   
-    <Canvas style={{ height: "100vh", background: "#000" }}>
-      <mesh
-        {...props}
-        ref={mesh}>
-        {props.component}
-        <ambientLight intensity={.1} />
-        <pointLight position={[10, 0, 7]} />
-        <OrbitControls autoRotate enableRotate={false} enableZoom={false} />
-      </mesh>
-    </Canvas>
+    <animated.mesh
+      {...props}
+      ref={mesh}>
+      {props.component}
+      <ambientLight intensity={.1} />
+      <pointLight position={[10, 0, 7]} />
+      {/* <OrbitControls autoRotate enableRotate={false} enableZoom={false} /> */}
+    </animated.mesh>
   )
 }

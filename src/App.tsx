aba70f344ac7f3f77a453bb6d4f540f00e88312f
Pengpa0012 <peng.ilikha@gmail.com>
2023-03-15 00:18:26 +0800

    add sample text

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,37 @@
 import { Canvas, useLoader } from '@react-three/fiber'
 import './App.css'
 import { Mesh } from './Components/Mesh'
-import { OrbitControls } from '@react-three/drei'
+import { Grid, OrbitControls } from '@react-three/drei'
 import { RockBasicColor, RockNormal, RockRoughness } from './assets'
 import { TextureLoader } from 'three'
-import { useEffect, useState } from 'react'
-import { config, useSpring } from '@react-spring/three'
-
 function App() {
   const [rockMap, rockNormal, rockRoughness] = useLoader(TextureLoader, [
     RockBasicColor,
     RockNormal,
     RockRoughness,
   ])
-  const [size, setSize] = useState(1)
-  const [active, setActive] = useState(false);
-
-  const { scale } = useSpring({
-    scale: size
-  });
   
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setSize(window.scrollY * 0.002 + 1)
-    }
-
-    window.addEventListener('wheel', handleScroll)
-
-    return () => {
-      window.removeEventListener('wheel', handleScroll)
-    }
-  }, [])
-  console.log(scale)
   // TODO
-  // Scroll event with model resizing
+  // Add model
+  // Control using arrow keys/mouse
+  // Add background
+  // Add different model
   return (
     <div className="App">
       <div className="relative">
-        <Canvas style={{ height: "100vh", background: "#000", position: "fixed" }}>
+        <Canvas style={{ height: "100vh", background: "#000" }}>
+          <gridHelper />
           <Mesh
-          onClick={() => setActive(!active)}
+            
             component={
             <>
-              <sphereGeometry args={[1, 50, 50]}  />
+              <sphereGeometry args={[1, 50, 50]} />
               <meshStandardMaterial normalMap={rockNormal} map={rockMap} roughnessMap={rockRoughness} />
+              <OrbitControls />
             </>
-          } position={[0, 0, 0]} scale={scale} />
+          } position={[2, 0, 2]} />
         </Canvas>
+        <h1 className="text-white font-bold text-6xl absolute top-[50%] -translate-y-[50%] left-[100px]">Sample Header Here <br /> test test</h1>
       </div>
       <div className="min-h-screen"></div>
       <div className="min-h-screen"></div>

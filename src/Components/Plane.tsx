641c558e7181c514ef78a8110caa77ef9ec9ae45
Pengpa0012 <peng.ilikha@gmail.com>
2023-03-22 09:33:30 +0800

    added reflection

--- src/Components/Plane.tsx
+++ src/Components/Plane.tsx
@@ -1,4 +1,5 @@
 import { usePlane } from '@react-three/cannon';
+import { MeshReflectorMaterial } from '@react-three/drei';
 import { useLoader } from '@react-three/fiber';
 import React, { useState } from 'react'
 import { DoubleSide, TextureLoader } from "three";
@@ -10,7 +11,23 @@ export const Plane = (props: any) => {
   return (
     <mesh ref={ref} receiveShadow>
       <planeGeometry args={[30, 30]} />
-      <meshStandardMaterial color="red" map={floorBase} normalMap={floorNormal} roughnessMap={floorRoughness}/>
+      <MeshReflectorMaterial color="red" map={floorBase} normalMap={floorNormal} roughnessMap={floorRoughness}  transparent={true}
+          envMapIntensity={0.35}
+          metalness={0.05}
+          roughness={0.4}
+          dithering={true}
+          blur={[1024, 512]} // Blur ground reflections (width, heigt), 0 skips blur
+          mixBlur={10} // How much blur mixes with surface roughness (default = 1)
+          mixStrength={5} // Strength of the reflections
+          mixContrast={1} // Contrast of the reflections
+          resolution={1024} // Off-buffer resolution, lower=faster, higher=better quality, slower
+          mirror={0} // Mirror environment, 0 = texture colors, 1 = pick up env colors
+          depthScale={0} // Scale the depth factor (0 = no depth, default = 0)
+          minDepthThreshold={0.9} // Lower edge for the depthTexture interpolation (default = 0)
+          maxDepthThreshold={1} // Upper edge for the depthTexture interpolation (default = 0)
+          depthToBlurRatioBias={0.25} // Adds a bias factor to the depthTexture before calculating the blur amount [bl
+          reflectorOffset={0.02} //
+          />
     </mesh>
   )
 }
